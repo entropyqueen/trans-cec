506c50f2a67df5fb17657c0d297d3d7a654d90bf
entropyQueen <emy.rg@protonmail.com>
2022-11-25 18:19:13 +0100

    remove session related stuff from config because we don't use them

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,8 +25,6 @@ LATEX_INTERPRETER = 'pdflatex'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_tex',
     'pdfgenerator',
@@ -34,10 +32,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
